132ad3953196f57965bdc0eebc8b5c4350113d03
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-03 22:01:47 -0300

    Feat: class inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+from typing import List, Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self._infos = data or list()
+
+    def add_data(self, data: List[Product]) -> None:
+        self._infos.extend(data)
+
+    @property
+    def data(self) -> List[Product]:
+        return self._infos
