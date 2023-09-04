972ed54a520b03c7ceb7a39d656c56a5815fa48a
Lenon Nascimento <lenoninformatica@gmail.com>
2023-09-03 21:54:48 -0300

    Feat: class importers

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,16 +1,43 @@
-from typing import Dict, Type
+from inventory_report.product import Product
+from abc import ABC, abstractmethod
+from typing import Dict, List, Type
+import json
+import csv
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
 
 
-class JsonImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> List[Product]:
+        novo_arquivo = list()
+
+        with open(self.path) as arquivo:
+            pdt = csv.DictReader(arquivo)
+
+            for x in pdt:
+                novo_arquivo.append(Product(**x))
+
+        return novo_arquivo
 
 
-class CsvImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        novo_arquivo = list()
+
+        with open(self.path) as arquivo:
+            pdt = json.load(arquivo)
+
+        for x in pdt:
+            novo_arquivo.append(Product(**x))
+
+        return novo_arquivo
 
 
 # Não altere a variável abaixo
